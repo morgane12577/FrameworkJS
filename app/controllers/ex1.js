a004114d2f4e3a07b9e538c4ddc48049b65fb9c3
morgane messager <22000880@etu.unicaen.fr>
2022-02-04 13:54:35 +0100

    TD1 fini

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -5,7 +5,6 @@ import { tracked } from '@glimmer/tracking';
 export default class Ex1Controller extends Controller {
   @tracked content = 'Entrez votre texte';
   @tracked info = '';
-  @tracked items = '';
   MAX = 100;
 
   get style() {
@@ -18,27 +17,11 @@ export default class Ex1Controller extends Controller {
     return this.MAX - this.content.length;
   }
 
-  get items() {
-    return this.items;
-  }
-
-  set items(val) {
-    this.items = val;
-  }
-
   @action
   clear() {
     this.content = '';
     this.info = '';
   }
-
-  /*@action
-  save() {
-    this.info = 'Note sauvegardée';
-    this.items = this.content.split('\n');
-    console.log(this.items);
-
-  }*/
 
   @action
   update() {
