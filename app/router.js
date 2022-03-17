3fad74e1040904dfde5df56498d9c1d9524db8c7
morgane messager <22000880@etu.unicaen.fr>
2022-03-17 13:31:28 +0100

    sedf

--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('test-new');
+  this.route('testList');
+});
