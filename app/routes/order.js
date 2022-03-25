dfab0fad1ecb8ee266f77c890bdcdee1a6e2fed4
morgane messager <22000880@etu.unicaen.fr>
2022-03-25 14:22:33 +0100

    board + début order/idOrder

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,8 +1,20 @@
 import Route from '@ember/routing/route';
+import Abstractroute from './Abstractroute';
+import RSVP from 'rsvp';
 
-export default class OrderRoute extends Route {
+export default class OrderRoute extends Abstractroute {
+@service store;
 
-  model(){
-    return {};
-  }
+model(params){
+   console.log("test");
+  console.log(params.order_id);
+  let idO = params.order_id;
+
+  let user = this.userAuth.user;
+
+  
+
+  return {idO, user};
+}
+
 }
